013afcdfeb9727750c2b07e8f9b0f4c8b144a656
datta1808 <bagaledatta18@gmail.com>
2021-07-09 17:10:42 +0530

    ADDED : Functionality to save Employee Payroll Object to Local Storage

--- js/EmployeePayrollForm.js
+++ js/EmployeePayrollForm.js
@@ -27,9 +27,22 @@ window.addEventListener('DOMContentLoaded', (event) => {
 const save = () => {
     try {
         let employeePayrollData = createEmployeePayroll();
+        createAndUpdateStorage(employeePayrollData);
     } catch (error) {
         return;
     }
+}
+
+
+function createAndUpdateStorage(employeePayrollData) {
+    let employeePayrollList = JSON.parse(localStorage.getItem("EmployeePayrollList"));
+    if(employeePayrollList!=undefined)
+        employeePayrollList.push(employeePayrollData);
+    else {
+        employeePayrollList = [employeePayrollData];
+    }
+    alert(employeePayrollList.toString());    
+    localStorage.setItem("EmployeePayrollList", JSON.stringify(employeePayrollList));
 }
 
 const createEmployeePayroll = () => {
